ba846144d21babfd920764bf3debccd77ec448b5
MAK1z <66070055@kmitl.ac.th>
2025-09-15 18:35:51 +0700

    UwU

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -3,7 +3,7 @@ num1 = int(input())
 print("Enter the second number: ")
 num2 = int(input())
 result = num1 * num2
-print(f"{num1} * {num2} = {result}")
+print(f"{num1} x {num2} = {result}")
 if result > 0:
     print("The result is positive")
 elif result < 0:
